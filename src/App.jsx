abf73d52430aaeb7ccfcf95334b7bbdf976e2f86
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-04-02 09:31:29 +0100

    feat: iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,88 @@
 import "./App.css";
 import contacts from "./contacts.json"
-const fiveContacts = contacts.slice(0,5);
+import { useState } from "react";
 
-function Contacts({name, pictureUrl, popularity, wonOscar, wonEmmy}){
- 
-    
-  return (
-    <>
-  <th scope="row"></th>    
-    <tr>
-      <td><img src={pictureUrl} alt="" /></td>
-      <td>{name}</td>
-      <td>{popularity.toFixed(2)}</td>
-      <td>{wonEmmy && <i className="fa fa-trophy"> </i>}</td>
-      <td>{wonOscar && <i className="fa fa-trophy"></i> }</td>
-    </tr>
-
-    </>
-     )
-}
 
 function App() {
+  const [numActors, setNumActors] = useState(5)
+  const [listContacts, setListContacts] = useState(contacts.slice(0, numActors));
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
-      
 
-      
-        <div>
-      <table className="table">
-  <thead>
-    <tr>
-      
-      <th scope="col">Picture</th>
-      <th scope="col">Name</th>
-      <th scope="col">Popularity</th>
-      <th scope="col">Won oscar</th>
-      <th scope="col">Won Emmy</th>
-    </tr>
-  </thead>
-  <tbody>
-    
-   
+      <ButtonAdd setNumActors={setNumActors} setListContacts={setListContacts} listContacts={listContacts} />
 
-      {fiveContacts.map((actor) => {
-        return (
-          <Contacts key={actor.id} name= {actor.name} pictureUrl={actor.pictureUrl} popularity={actor.popularity} wonEmmy={actor.wonEmmy} wonOscar={actor.wonOscar}/>
-        )
-      })}
-    
-  </tbody>
-</table>
-      
-    </div> 
-      
-      
-      
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+
+              <th scope="col">Picture</th>
+              <th scope="col">Name</th>
+              <th scope="col">Popularity</th>
+              <th scope="col">Won oscar</th>
+              <th scope="col">Won Emmy</th>
+            </tr>
+          </thead>
+          <tbody>
+
+
+
+            {listContacts.map((actor) => {
+              return (
+                <Contacts key={actor.id} name={actor.name} pictureUrl={actor.pictureUrl} popularity={actor.popularity} wonEmmy={actor.wonEmmy} wonOscar={actor.wonOscar} />
+              )
+            })}
+
+          </tbody>
+        </table>
+
+      </div>
+
+
+
     </div>
   );
 }
+function Contacts({ name, pictureUrl, popularity, wonOscar, wonEmmy }) {
+
+
+  return (
+    <>
+      <th scope="row"></th>
+      <tr>
+        <td><img src={pictureUrl} alt="" /></td>
+        <td>{name}</td>
+        <td>{popularity.toFixed(2)}</td>
+        <td>{wonEmmy && <i className="fa fa-trophy"> </i>}</td>
+        <td>{wonOscar && <i className="fa fa-trophy"></i>}</td>
+      </tr>
+
+    </>
+  )
+}
+
+
+function ButtonAdd({ setNumActors, listContacts }) {
+
+  return (
+    <div>
+      <button onClick={() => {
+
+        setNumActors(prevNumActors => prevNumActors + 1);
+        let randomNum = Math.floor(Math.random() * (contacts.length - 5) + 5);
+        let newActor = contacts[randomNum];
+        console.log(listContacts)
+
+        listContacts.push(newActor)
 
 
 
+      }}>Add Random Contacts</button>
+    </div>
+  )
+}
 
 
 
